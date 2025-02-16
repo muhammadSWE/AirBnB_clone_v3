9ecdf5cef8b0ffc8bac0e6f0b45512673a442f7a
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-02-16 23:32:47 +0200

    Refactor import statements to enhance module structure and organization

---
+++ models/engine/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""initialize the models package"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import classes
+from models.engine import classes
 
 
 class FileStorage:
