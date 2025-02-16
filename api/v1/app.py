fb4723f8d36dcc80610ff00bd0f9f61b443bd281
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-02-16 22:38:06 +0200

    Add shebang and improve code formatting in API files for consistency

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Call storage.close() on teardown of the Flask app context"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Blueprint for the API."""
 from flask import Blueprint
 
