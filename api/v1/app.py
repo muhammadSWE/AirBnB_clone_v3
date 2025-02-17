83f432347046c7821e8cef4f30328cf70c098cf2
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2025-02-18 01:30:48 +0200

    Update app configuration to use environment variables for host and port

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,12 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from os import environ
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -25,4 +26,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = environ.get('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
